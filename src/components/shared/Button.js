b689a93de093f32360b650376fdd2d6a6225f248
Lewis Horwood <horwood.lewis@gmail.com>
2018-12-25 18:47:19 +0900

    add media query on font-size

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -6,11 +6,14 @@ const Button = styled(Link)`
   border-radius: 2px;
   box-shadow: 2px 2px ${props => props.theme.colors.activeShade};
   color: ${props => props.theme.colors.bg1};
-  font-size: 24px;
+  font-size: 120%;
   margin: 1rem auto;
   padding: 6px;
   text-align: center;
   text-decoration: none;
+  @media screen and (min-width: 1024px) {
+    font-size: 150%;
+  }
 `
 
 export default Button
