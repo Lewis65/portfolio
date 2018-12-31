0cfeeef70018fe34523a02242ea6ce71ea8c8a69
Lewis Horwood <horwood.lewis@gmail.com>
2018-12-31 19:11:14 +0900

    style project card titles

--- src/components/Portfolio/ProjectCard.js
+++ src/components/Portfolio/ProjectCard.js
@@ -7,28 +7,41 @@ const Card = styled.div`
   box-shadow: 5px 5px ${props => props.theme.colors.bg2};
   box-sizing: border-box;
   cursor: pointer;
+  display: flex;
+  flex-direction: column;
   font-size: 75%;
   width: 90%;
-  margin: 0.5rem;
-  min-height: 150px;
-  padding: 0.5rem;
+  margin: 1rem;
+  padding: 0;
   user-select: none;
   -ms-user-select: none;
   -moz-user-select: none;
   -webkit-user-select: none;
   @media screen and (min-width: 1024px) {
     font-size: 90%;
-    margin: 1rem;
-    padding: 1rem;
+    margin: 2rem;
     width: 33%;
   }
 `
+
+const ProjectTitle = styled.h3`
+  background-color: ${props => props.theme.colors.highlight};
+  color: ${props => props.theme.colors.bg2};
+  display: block;
+  font-size: 125%;
+  margin: 0;
+  padding: 0.5rem;
+`
+
+//Add clickable tags, thumbnails, then work on project detail modal
 
 const ProjectCard = (props) => {
 
   return(
     <Card onClick={()=>props.handleCardClick(props.projectId)}>
-      {props.project.title}
+      <ProjectTitle >
+        {props.project.title}
+      </ProjectTitle>
     </Card>
   )
 }
