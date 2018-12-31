41cea70995d8edf3b61775674478bf6c574bfc4d
Lewis Horwood <horwood.lewis@gmail.com>
2018-12-31 16:49:06 +0900

    add bottom margin

--- src/components/shared/Heading.js
+++ src/components/shared/Heading.js
@@ -7,11 +7,12 @@ const Heading = styled.h1`
   display: inline-block;
   font-family: ${props => props.theme.fonts.header};
   font-size: 48px;
-  margin: 0 auto 0 0;
+  margin: 0 auto 1rem 0;
   padding: 1rem;
   @media screen and (min-width: 1024px) {
     box-shadow: 8px 8px ${props => props.active ? props.theme.colors.activeShade : props.theme.colors.highlight};
     font-size: 500%;
+    margin-bottom: 2rem;
   }
 `
 
