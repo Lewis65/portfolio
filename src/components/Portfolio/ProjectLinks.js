b3b0fef277218f7cc360b57173c562d537f7cac2
Lewis Horwood <horwood.lewis@gmail.com>
2019-01-17 22:06:03 +0900

    create ProjectLinks component

---
+++ src/components/Portfolio/ProjectLinks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+//Create link group for 2-3 links using different preset icons depending on their anchor text
+const ProjectLinks = () => {
+
+}
+
+export default ProjectLinks
