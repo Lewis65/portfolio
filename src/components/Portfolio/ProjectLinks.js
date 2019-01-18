b5c23de82e5dd59c9d0ac4b0727437c066ef6bf6
Lewis Horwood <horwood.lewis@gmail.com>
2019-01-18 17:10:04 +0900

    change ProjectLinks to external/internal agnostic Link from Gatsby docs

--- src/components/Portfolio/ProjectLinks.js
+++ src/components/Portfolio/ProjectLinks.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'gatsby';
+import Link from '../shared/Link'
 
 //classNames for relevant fontAwesome icons
 const icons = {
