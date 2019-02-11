4f33c9cd5b03cf87959f9f9773400b15889012c1
Lewis Horwood <horwood.lewis@gmail.com>
2019-02-11 13:19:28 +0900

    add conditional styles for Blog headings

--- src/components/shared/Heading.js
+++ src/components/shared/Heading.js
@@ -1,17 +1,17 @@
 import styled from 'styled-components'
 
 const Heading = styled.h1`
-  background-color: ${props => props.hero ? "white" : props.theme.colors.bg2};
-  box-shadow: 8px 8px ${props => props.hero ?  props.theme.colors.active : props.theme.colors.shadow};
-  color: ${props => props.hero ? props.theme.colors.highlight : props.theme.colors.body};
+  background-color: ${props => props.hero || props.blog ? "white" : props.theme.colors.bg2};
+  box-shadow: 8px 8px ${props => props.hero || props.blog ?  props.theme.colors.active : props.theme.colors.shadow};
+  color: ${props => props.hero || props.blog ? props.theme.colors.highlight : props.theme.colors.body};
   display: inline-block;
   font-family: ${props => props.theme.fonts.header};
-  font-size: 56px;
+  font-size: ${props => props.blog ? "200%" : "56px"};
   margin: 0 auto 1rem 0;
   padding: 1rem;
   @media screen and (min-width: 1024px) {
     font-size: 500%;
-    margin-bottom: 2rem;
+    margin-bottom: ${props => props.hero ? "2rem" : 0};
   }
 `
 
