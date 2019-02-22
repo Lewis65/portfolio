eb94ec95bb22dbd48b2311d4ece065b8c18f67b1
Lewis Horwood <horwood.lewis@gmail.com>
2019-02-22 09:39:22 +0900

    add pageQuery for listing all posts (draft)

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,15 +5,36 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Main from '../components/shared/MainPage'
 import Heading from '../components/shared/Heading'
+import { graphql } from 'gatsby';
 
-const BlogPage = () => (
-  <Layout>
+const BlogPage = (props) => {
+  return <Layout>
     <SEO title="Blog" keywords={['Lewis Horwood', 'web developer', 'react', 'Seoul', 'Calgary', 'javascript developer', 'programmer', 'software engineer', 'frontend']} />
     <Main>
       <Heading>Blog</Heading>
       
     </Main>
   </Layout>
-)
+}
 
 export default BlogPage
+
+export const pageQuery = graphql`
+  {
+    allContentfulBlogPost {
+      edges {
+        node {
+          id
+          title
+          
+          description {
+            description
+          }
+          bodyText {
+            bodyText
+          }
+        }
+      }
+    }
+  }
+`
