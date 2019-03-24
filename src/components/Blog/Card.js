f9d2f142aee287bd40f53c1b7f52c8cea6f239bd
Lewis Horwood <horwood.lewis@gmail.com>
2019-03-24 15:37:15 +0900

    restyle Blog Cards

--- src/components/Blog/Card.js
+++ src/components/Blog/Card.js
@@ -5,43 +5,59 @@ import Img from 'gatsby-image'
 import moment from 'moment'
 
 const CardDate = styled.div`
-    background-color: ${props => props.theme.colors.darkblue};
     box-sizing: border-box;
-    color: white;
-    padding: 1rem 2rem;
+    color: ${props => props.theme.colors.pink};
+    padding: 1rem 2rem 0;
     text-align: right;
-    text-shadow: 2px 2px 4px black;
     width: 100%;
 `
 
 const CardDescription = styled.div`
     box-sizing: border-box;
-    color: white;
-    height: 0;
-    padding: 0 2rem;
-    opacity: 0;
-    text-shadow: 2px 2px 4px black;
+    padding: 1rem 2rem;
     transition: opacity 0.15s ease-out, padding 0.15s ease-out;
 `
 
 const CardImage = styled(Img)`
     margin: 0;
     width: 100%;
+    z-index: 1;
+`
+
+const CardInfo = styled.div`
+    position: relative;
+`
+
+const CardMeta = styled.div`
+    &.img {
+        position: absolute;
+        text-shadow: 1px 1px 2px black;
+        top: 0;
+        z-index: 2;
+        .description {
+            color: white;
+        }
+    }
+    &.noimg {
+        .description {
+            color: ${props => props.theme.colors.body};
+        }
+    }
 `
 
 const CardTitle = styled.h2`
-    background-color: ${props => props.theme.colors.pink};
+    background-image: linear-gradient(130deg, ${props => props.theme.colors.darkblue}, ${props => props.theme.colors.pink});
     box-sizing: border-box;
     color: white;
     margin: 0;
-    padding: 0.5rem 2rem;
-    text-shadow: 3px 3px 4px ${props => props.theme.colors.shadow};
-    transition: color 0.15s ease-out;
+    padding: 1rem 2rem 0.5rem;
+    text-shadow: 3px 3px 6px ${props => props.theme.colors.shadow};
+    transition: color 0.15s ease-out, text-shadow 0.15s ease-out;
     width: 100%;
 `
 
 const CardWrapper = styled(Link)`
-    background-color: ${props => props.theme.colors.darkblue};
+    background-color: white;
     border-radius: 1rem;
     box-shadow: ${props => props.theme.smallShadow};
     box-sizing: border-box;
@@ -60,29 +76,32 @@ const CardWrapper = styled(Link)`
     -webkit-user-select: none;
     &:hover {
         opacity: 1;
-        .description {
-            height: auto;
-            opacity: 1;
-            padding: 1rem 2rem;
+        h2 {
+            text-shadow: 3px 3px 6px ${props => props.theme.colors.pink};
         }
     }
 `
 
 const Card = (props) => {
     return <CardWrapper to={`/blog/${props.post.slug}`}>
-        
-            <CardDate>
-                {moment(props.post.createdAt).fromNow()}
-            </CardDate>
             <CardTitle>
                 {props.post.title}
             </CardTitle>
-            {props.post.featuredImage ? 
-                <CardImage fluid={props.post.featuredImage.fluid}/> : null
-            }
-            <CardDescription className="description">
-                {props.post.description.description}
-            </CardDescription>
+            <CardInfo>
+                {
+                    props.post.featuredImage ? 
+                    <CardImage fluid={props.post.featuredImage.fluid}/> : 
+                    null
+                }
+                <CardMeta className={props.post.featuredImage ? "img" : "noimg"}>
+                    <CardDate>
+                        {moment(props.post.createdAt).fromNow()}
+                    </CardDate>
+                    <CardDescription className="description">
+                        {props.post.description.description}
+                    </CardDescription>
+                </CardMeta>
+            </CardInfo>
         
     </CardWrapper>
 }
