1e7b15a1de4eda542e8862583bb1bdc10e95fecc
Lewis Horwood <horwood.lewis@gmail.com>
2019-05-20 20:09:54 +0900

    simplify styling

--- src/components/About/Hero.js
+++ src/components/About/Hero.js
@@ -1,26 +1,22 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import Image from 'gatsby-image'
 import HeroImgSrc from '../../images/hero1.jpg'
 
-const HeroWrapper = styled.div`
+const HeroImage = styled.div`
   background-attachment: fixed;
   background-image: url(${HeroImgSrc});
-  background-position: bottom center;
+  background-position: center;
   background-size: cover;
   top: 0;
   left: 0;
   bottom: 0;
   right: 0;
   height: 100vh;
-  max-width: 100vw;
-  position: absolute;
-  width: 100%;
+  position: fixed;
+  width: 100vw;
   z-index: 1;
-  @media screen and (min-width: 1024px) {
-    left: ${props => props.theme.headerWidth};
-    width: calc(100vw - ${props => props.theme.headerWidth});
-  }
 `
 
 const HeroShade = styled.div`
@@ -30,18 +26,14 @@ const HeroShade = styled.div`
   bottom: 0;
   right: 0;
   height: 100vh;
-  position: absolute;
+  position: fixed;
   width: 100vw;
   z-index: 2;
-  @media screen and (min-width: 1024px) {
-    left: ${props => props.theme.headerWidth};
-    width: calc(100vw - ${props => props.theme.headerWidth});
-  }
 `
 
 const Hero = () => (
   <React.Fragment>
-    <HeroWrapper/>
+    <HeroImage/>
     <HeroShade/>
   </React.Fragment>
 )
