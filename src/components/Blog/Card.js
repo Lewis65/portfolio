eda685cc5e2b2517f519542dfe3fafe6ae5bca9a
Lewis <horwood.lewis@gmail.com>
2019-08-06 14:37:42 +0900

    remove unused import

--- src/components/Blog/Card.js
+++ src/components/Blog/Card.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 import React from 'react'
-import { Link } from 'gatsby';
-import Img from 'gatsby-image'
+import { Link } from 'gatsby'
 import moment from 'moment'
 
 import Tags from '../shared/Tags'
