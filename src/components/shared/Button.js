5a2ab302721303a2f2364cf5e28887232285aa6d
Lewis Horwood <horwood.lewis@gmail.com>
2019-09-16 14:28:51 -0600

    restyle buttons

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,23 +1,18 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
-  background-color: ${props => props.theme.colors.pink};
+  background-color: ${props => props.theme.colors.lightblue};
   border: none;
-  border-radius: 1rem;
   box-shadow: ${props => props.theme.smallShadow};
   color: white;
+  cursor: pointer;
   font-size: 120%;
-  margin: 1rem auto;
   outline: none;
-  padding: 6px;
+  padding: 0.5rem;
   text-align: center;
   text-decoration: none;
   @media screen and (min-width: 1024px) {
     font-size: 150%;
-    transition: background-color 0.15s ease-out;
-    &:hover {
-      background-color: ${props => props.theme.colors.red};
-    }
   }
 `
 
--- src/components/shared/ButtonLink.js
+++ src/components/shared/ButtonLink.js
@@ -2,13 +2,13 @@ import styled from 'styled-components'
 import { Link } from 'gatsby'
 
 const Button = styled(Link)`
-  background-color: ${props => props.theme.colors.pink};
-  border-radius: 1rem;
+  background-color: ${props => props.theme.colors.lightblue};
+  border: none;
   box-shadow: ${props => props.theme.smallShadow};
   color: white;
+  cursor: pointer;
   font-size: 120%;
-  margin: 1rem auto;
-  padding: 6px;
+  padding: 0.5rem;
   text-align: center;
   text-decoration: none;
   @media screen and (min-width: 1024px) {
