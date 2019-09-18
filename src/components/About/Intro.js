9ecca0badb1b83e80de508a990293d8a521be11f
Lewis Horwood <horwood.lewis@gmail.com>
2019-09-18 10:34:54 -0600

    rewrite introduction and add links

--- src/components/About/Intro.js
+++ src/components/About/Intro.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
+import Link from 'gatsby'
 import Heading from '../shared/Heading'
-import Button from '../shared/ButtonLink'
+import ButtonLink from '../shared/ButtonLink'
 import Section from '../About/Section'
 
 const Intro = () => (
     <Section className='about-section'>
         <Heading hero>Hi. I'm Lewis.</Heading>
-        <p>I'm a Javascript developer focused on creating web stuff that serves the needs of others and the things I find fascinating.</p>
-        <Button to='#'>Resume</Button>
+        <p>I'm a Javascript developer in Calgary, AB. I write apps and websites with React, SASS/styled-components, Node.js, Express,  and more.</p>
+        <p>Here you can see selected <Link to='/projects'>projects</Link> I've completed, as well as my <Link to ='/blog'>blog</Link>. You can also <Link to='/contact'>contact me</Link> or view <Link to='#'>my resume</Link>.</p>
+        
     </Section>
 )
 
